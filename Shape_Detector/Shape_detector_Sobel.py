501973c741dd2e754782ada4dbce89bd5e880f3e
Flo <florian.sauzeat@students.hevs.ch>
2025-03-07 13:05:49 +0100

    Une méthode qui marche !!!

--- Shape_Detector/Shape_detector_Sobel.py
+++ Shape_Detector/Shape_detector_Sobel.py
@@ -5,7 +5,7 @@ import numpy as np
 WINDOW_SIZE = (900, 700)
 
 # Charger l'image
-image_path = '../images/p1/WIN_20250306_15_09_28_Pro.jpg'
+image_path = '../images/p1_b/Natel.Black1.jpg'
 assert os.path.exists(image_path), f"Erreur : le fichier {image_path} n'existe pas."
 im = cv.imread(image_path)
 assert im is not None, "Erreur : l'image ne peut pas être lue."
@@ -13,12 +13,6 @@ assert im is not None, "Erreur : l'image ne peut pas être lue."
 # 6. Conversion en niveaux de gris pour la détection des contours
 gray = cv.cvtColor(im, cv.COLOR_BGR2GRAY)
 cv.imshow('gray', cv.resize(gray, WINDOW_SIZE))
-cv.waitKey(0)
-cv.destroyAllWindows()
-
-# Seuillage pour la détection des contours
-ret2, th3 = cv.threshold(gray, 160, 255, cv.THRESH_BINARY_INV)
-cv.imshow('Seuillage', cv.resize(th3, WINDOW_SIZE))
 cv.waitKey(0)
 cv.destroyAllWindows()
 
@@ -36,50 +30,57 @@ cv.imshow('Sobel', cv.resize(sobel, WINDOW_SIZE))
 cv.waitKey(0)
 cv.destroyAllWindows()
 
-#https://docs.opencv.org/4.x/d9/d61/tutorial_py_morphological_ops.html
-# Appliquer une fermeture morphologique
+
+
+# Seuillage automatique (Otsu) sur la magnitude du gradient
+# Appliquer un seuillage automatique avec Otsu après Sobel
+ret, th_sobel_otsu = cv.threshold(sobel, 0, 255, cv.THRESH_BINARY + cv.THRESH_OTSU)
+cv.imshow('Seuillage Sobel Otsu', cv.resize(th_sobel_otsu, WINDOW_SIZE))
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+# Appliquer une fermeture morphologique après Sobel (facultatif)
 kernel_closing_sobel = np.ones((3, 3), np.uint8)
 closing_sobel = cv.morphologyEx(sobel, cv.MORPH_CLOSE, kernel_closing_sobel)
 cv.imshow('Sobel+Closing', cv.resize(closing_sobel, WINDOW_SIZE))
 cv.waitKey(0)
 cv.destroyAllWindows()
 
-# Seuillage pour la détection des contours
-ret3, th4 = cv.threshold(closing_sobel, 45, 255, cv.THRESH_BINARY)
-cv.imshow('Seuillage', cv.resize(th4, WINDOW_SIZE))
-cv.waitKey(0)
-cv.destroyAllWindows()
+opening = cv.morphologyEx(closing_sobel, cv.MORPH_OPEN,kernel_closing_sobel)
 
-
-
-# Détection des contours
-contours, _ = cv.findContours(th4, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
+# Détection des contours après Sobel avec Otsu
+contours, _ = cv.findContours(th_sobel_otsu, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_NONE)
 im_contours = im.copy()
 cv.drawContours(im_contours, contours, -1, (0, 255, 0), 2)
-cv.imshow('Contours détectés', cv.resize(im_contours, WINDOW_SIZE))
+cv.imshow('Contours détectés après Sobel Otsu', cv.resize(im_contours, WINDOW_SIZE))
 cv.waitKey(0)
 cv.destroyAllWindows()
 
-'''
-laplacian = cv.Laplacian(gray, cv.CV_64F, ksize=3)
-laplacian = cv.convertScaleAbs(laplacian)
+# Filtrer les contours selon leur superficie
+min_area = 500  # Définir un seuil minimum pour la superficie des contours
+contours_filtered = [cnt for cnt in contours if cv.contourArea(cnt) > min_area]
 
-cv.imshow('Laplacian',cv.resize(laplacian,WINDOW_SIZE))
-closing_laplacian = cv.morphologyEx(laplacian, cv.MORPH_CLOSE, kernel)
-cv.imshow('laplacian+Closing', cv.resize(closing_laplacian,WINDOW_SIZE))
-cv.waitKey(0)
-#cv.destroyAllWindows()
-
-edges = cv.Canny(gray, 100, 200)  # Seuils bas et haut
-
-cv.imshow('Canny', cv.resize(edges,WINDOW_SIZE))
+# Dessiner les contours filtrés
+im_contours_filtered = im.copy()
+cv.drawContours(im_contours_filtered, contours_filtered, -1, (0, 255, 0), 2)
+cv.imshow('Contours filtrés', cv.resize(im_contours_filtered, WINDOW_SIZE))
 cv.waitKey(0)
 cv.destroyAllWindows()
 
-#https://docs.opencv.org/4.x/d9/d61/tutorial_py_morphological_ops.html
+# Laplacian (optionnel, non utilisé mais inclus dans le commentaire)
+# laplacian = cv.Laplacian(gray, cv.CV_64F, ksize=3)
+# laplacian = cv.convertScaleAbs(laplacian)
+# cv.imshow('Laplacian', cv.resize(laplacian, WINDOW_SIZE))
 
-closing = cv.morphologyEx(edges, cv.MORPH_CLOSE, kernel)
-cv.imshow('Canny+Closing', cv.resize(closing,WINDOW_SIZE))
-cv.waitKey(0)
-cv.destroyAllWindows()
-'''
+
+# Appliquer Canny (optionnel, également dans le commentaire)
+# edges = cv.Canny(gray, 100, 200)
+# cv.imshow('Canny', cv.resize(edges, WINDOW_SIZE))
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+# Appliquer la fermeture morphologique sur Canny (optionnel)
+# closing = cv.morphologyEx(edges, cv.MORPH_CLOSE, kernel)
+# cv.imshow('Canny+Closing', cv.resize(closing, WINDOW_SIZE))
+# cv.waitKey(0)
+# cv.destroyAllWindows()
